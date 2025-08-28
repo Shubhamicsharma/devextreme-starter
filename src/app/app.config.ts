40ba2fbde63bbb7c6d57681733ec9431d278f806
Shubhamicsharma <shubhamicsharma@gmail.com>
2025-08-28 17:34:35 +0530

    Initial Commit

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,5 +1,20 @@
 import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
+import { provideRouter, withHashLocation } from '@angular/router';
+import { routes } from './app.routes';
+import {
+    AppInfoService,
+    AuthGuardService,
+    AuthService,
+    ScreenService,
+} from './shared/services';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideZoneChangeDetection({ eventCoalescing: true })]
+  providers: [
+    provideZoneChangeDetection({ eventCoalescing: true }),
+    provideRouter(routes, withHashLocation()),
+    AuthGuardService,
+    AuthService,
+    ScreenService,
+    AppInfoService,
+  ]
 };
