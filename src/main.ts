c6dc1dc310f878944d593d604d129952be6482ff
Shubhamicsharma <shubhamicsharma@gmail.com>
2025-08-29 11:56:24 +0530

    Add license key configuration for DevExtreme

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,13 @@ import { bootstrapApplication } from '@angular/platform-browser';
 import { appConfig } from './app/app.config';
 import { AppComponent } from './app/app.component';
 
+import { licenseKey } from './devextreme-license'; // Import the license key
+import config from 'devextreme/core/config';
+
+config({
+    licenseKey: licenseKey
+})
+
 themes.initialized(() => {
   bootstrapApplication(AppComponent, appConfig)
     .catch((err) => console.error(err));
